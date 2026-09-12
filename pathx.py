9335216a22bc177be7f9ed0e07e2ff6117f4cb7a
Mindless <cyberhamid.pk@gmail.com>
2026-09-12 15:06:08 +0500

    Update pathx.py

--- pathx.py
+++ pathx.py
@@ -7,7 +7,7 @@
 ██╔═══╝ ██╔══██║   ██║   ██╔══██║ ██╔██╗
 ██║     ██║  ██║   ██║   ██║  ██║██╔╝ ██╗
 ╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝
-qdxew
+qdxewww
 PathX — Directory & Endpoint Discovery Scanner
 Made by Mindless — Founder & CEO of Linxploit
 https://linxploit.com | https://linxploit.com/founder
