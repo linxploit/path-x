e115d2c7938a56cda6af31fbc83cab7365dbf6ff
Mindless <f25609043@nutech.edu.pk>
2026-09-12 15:07:09 +0500

    Update pathx.py

--- pathx.py
+++ pathx.py
@@ -7,7 +7,7 @@
 ██╔═══╝ ██╔══██║   ██║   ██╔══██║ ██╔██╗
 ██║     ██║  ██║   ██║   ██║  ██║██╔╝ ██╗
 ╚═╝     ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝
-qdxewwwewqf
+qdxewwwewqfwqedq
 PathX — Directory & Endpoint Discovery Scanner
 Made by Mindless — Founder & CEO of Linxploit
 https://linxploit.com | https://linxploit.com/founder
